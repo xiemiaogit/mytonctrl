fffc39e61f6b6ffdbabd6711db59948192559043
xiemiaogit <277122931@qq.com>
2024-04-29 06:51:53 +0800

    Update toninstaller.sh

--- scripts/toninstaller.sh
+++ scripts/toninstaller.sh
@@ -135,7 +135,7 @@ rm -rf $SOURCES_DIR/mytonctrl
 # cp -r $SOURCES_DIR/ton_bak $SOURCES_DIR/ton
 # cp -r $SOURCES_DIR/mytonctrl_bak $SOURCES_DIR/mytonctrl
 https_proxy="http://127.0.0.1:8118" git clone --recursive https://github.com/ton-blockchain/ton.git
-https_proxy="http://127.0.0.1:8118" git clone --recursive https://github.com/xiemiaogit/mytonctrl.git
+https_proxy="http://127.0.0.1:8118" git clone --recursive https://github.com/ton-blockchain/mytonctrl.git
 git config --global --add safe.directory $SOURCES_DIR/ton
 git config --global --add safe.directory $SOURCES_DIR/mytonctrl
 
